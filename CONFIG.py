c97c188d8d4cb4db21c106debf9ab1ddf61defa7
PranitJadhav11 <pranitj337@gmail.com>
2023-03-23 15:07:39 +0530

    Path Updation

--- CONFIG.py
+++ CONFIG.py
@@ -2,6 +2,6 @@ HOST = "0.0.0.0"
 PORT = 8000
 DEBUG = False
 
-MODEL_PATH =r"/Car_Pred_Model.pkl"
-COLUMNS_PATH = r"/Column.json"
-ENCODE_PATH = r"/encoded_data.json"
+MODEL_PATH =r"/home/ubuntu/car_selling_price_prediction/app/Car_Pred_Model.pkl"
+COLUMNS_PATH = r"/home/ubuntu/car_selling_price_prediction/app/Column.json"
+ENCODE_PATH = r"/home/ubuntu/car_selling_price_prediction/app/encoded_data.json"
