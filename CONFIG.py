21a83bc83b70728be80b7a195a3e3887219527b2
PranitJadhav11 <pranitj337@gmail.com>
2023-03-23 15:05:45 +0530

    Path Updation

--- CONFIG.py
+++ CONFIG.py
@@ -2,6 +2,6 @@ HOST = "0.0.0.0"
 PORT = 8000
 DEBUG = False
 
-MODEL_PATH =r".\Car_Pred_Model.pkl"
-COLUMNS_PATH = r".\Column.json"
-ENCODE_PATH = r".\encoded_data.json"
+MODEL_PATH =r"/Car_Pred_Model.pkl"
+COLUMNS_PATH = r"/Column.json"
+ENCODE_PATH = r"/encoded_data.json"
