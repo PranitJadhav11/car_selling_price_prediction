1d32adce855bdf93de868f212395a452a475a4f3
Pranit S. Jadhav <127429122+PranitJadhav11@users.noreply.github.com>
2023-05-22 09:44:08 +0530

    path changed

--- CONFIG.py
+++ CONFIG.py
@@ -2,6 +2,6 @@ HOST = "0.0.0.0"
 PORT = 8000
 DEBUG = False
 
-MODEL_PATH =r"/home/ubuntu/car_selling_price_prediction/app/Car_Pred_Model.pkl"
-COLUMNS_PATH = r"/home/ubuntu/car_selling_price_prediction/app/Column.json"
-ENCODE_PATH = r"/home/ubuntu/car_selling_price_prediction/app/encoded_data.json"
+MODEL_PATH =r"app/Car_Pred_Model.pkl"
+COLUMNS_PATH = r"app/Column.json"
+ENCODE_PATH = r"app/encoded_data.json"
